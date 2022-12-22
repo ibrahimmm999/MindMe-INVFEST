b33c5956a506c45fafc197087a484006050cc290
Ibrahim Imam <ibrhmmm552@gmail.com>
2022-12-22 18:25:09 +0700

    update model service cubit

--- src/lib/models/user_model.dart
+++ src/lib/models/user_model.dart
@@ -1,28 +1,39 @@
 import 'package:equatable/equatable.dart';
+import 'package:src/models/article_model.dart';
 import 'package:src/models/video_model.dart';
 
 class UserModel extends Equatable {
   final String id;
   final String email;
-  final String fullname;
+  final String name;
   final String role;
   final String username;
   final bool isPremium;
   final String photoUrl;
-  final Map bookmark_article;
+  final List<ArticleModel> bookmark_article;
   final List<VideoModel> bookmark_video;
 
   UserModel(
       {required this.id,
-      required this.role,
+      this.role = 'user',
       required this.email,
-      required this.fullname,
+      required this.name,
       required this.username,
-      required this.isPremium,
+      this.isPremium = false,
       this.photoUrl = '',
       required this.bookmark_article,
       required this.bookmark_video});
   @override
   // TODO: implement props
-  List<Object?> get props => [id, email, fullname, username, isPremium];
+  List<Object?> get props => [
+        id,
+        email,
+        name,
+        username,
+        isPremium,
+        photoUrl,
+        role,
+        bookmark_article,
+        bookmark_video
+      ];
 }
---
+++ src/lib/services/auth_service.dart
@@ -0,0 +1,53 @@
+import '../models/user_model.dart';
+import '../services/user_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      UserModel user =
+          await UserService().getUserById(userCredential.user!.uid);
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<UserModel> signUp(
+      {required String email,
+      required String password,
+      required String name,
+      required String username}) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      UserModel user = UserModel(
+          id: userCredential.user!.uid,
+          email: email,
+          name: name,
+          username: username,
+          bookmark_article: [],
+          bookmark_video: []);
+      await UserService().setUser(user);
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      throw e;
+    }
+  }
+}
